33912a211f28fcd01cb18ab3af1b3ef472905861
Clayton5456 <71726374+Clayton5456@users.noreply.github.com>
2022-10-01 17:10:52 -0500

    Got work up to date

--- Week 5/clmg44.js
+++ Week 5/clmg44.js
@@ -1,3 +1,6 @@
+var state=0;
+
+
 function switchy1(){
     document.getElementById("youtube").src="https://www.youtube.com/embed/mACKHGfdLlo?autoplay=1";
     
@@ -12,19 +15,73 @@ function switchy3(){
     document.getElementById("youtube").src="https://www.youtube.com/embed/2RPjK6EwfCI?autoplay=1"
 }
 function switchy4(){
-
+    document.getElementById("youtube").src="https://www.youtube.com/embed/xuCn8ux2gbs?autoplay=1"
 }
 
 function switchv1(){
+    document.getElementById("playn").src="./videos/FinishedShape.mp4";
+    document.getElementById("source").type="video/mp4";
+    document.getElementById("control").innerHTML="Play";
+    state=0;
+
 
 }
 
 function switchv2(){
+    document.getElementById("playn").src="./videos/maskLiner.mp4";
+    document.getElementById("control").innerHTML="Play";
+    state=0;
+    
+    
 
 }
 
 function switchv3(){
+    document.getElementById("playn").src="./videos/PartAFinishedShape.mp4";
+    document.getElementById("control").innerHTML="Play";
+    state=0;
+    
 
+}
+const video = new Map();
+
+video.set("Big",1000);
+video.set("Normal",500);
+video.set("Small",100);
+
+function size1(){
+    document.getElementById("playn").width=video.get("Small");
+    
+}
+
+function size2(){
+    document.getElementById("playn").width=video.get("Normal");
+    
+
+}
+
+function size3(){
+    document.getElementById("playn").width=video.get("Big");
+    
+
+}
+
+function controls(){
+    
+    if(state==0){
+        document.getElementById("playn").play();
+        document.getElementById("control").innerHTML="Pause";
+        state=1;
+        
+    }
+    else if(state==1){
+        document.getElementById("playn").pause();
+        document.getElementById("control").innerHTML="Play";
+        state=0;
+    }
+    else{
+        alert("Something went wrong!");
+    }
 }
 
 
