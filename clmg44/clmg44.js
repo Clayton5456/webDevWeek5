2e3ec82a1184d10822c0658c215d1b55253b61c5
Clayton5456 <71726374+Clayton5456@users.noreply.github.com>
2022-09-30 19:29:51 -0500

    First copying in

---
+++ clmg44/clmg44.js
@@ -0,0 +1,30 @@
+function switchy1(){
+    document.getElementById("youtube").src="https://www.youtube.com/embed/mACKHGfdLlo?autoplay=1";
+    
+}
+
+
+function switchy2(){
+    document.getElementById("youtube").src="https://www.youtube.com/embed/tqsy9Wtr1qE?autoplay=1";
+}
+
+function switchy3(){
+    document.getElementById("youtube").src="https://www.youtube.com/embed/2RPjK6EwfCI?autoplay=1"
+}
+function switchy4(){
+
+}
+
+function switchv1(){
+
+}
+
+function switchv2(){
+
+}
+
+function switchv3(){
+
+}
+
+
